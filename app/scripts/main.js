d58f3c0785f5bd249e149ff167b48dd6d0c96989
Kyle Kopicki <kyle.kopicki@gmail.com>
2014-10-20 12:55:07 -0400

    setting up require for dependencies.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,61 +1,58 @@
-(function ( $ ) {
-  'use strict';
-  var o = $({});
+require(['jquery', 'underscore'], function ($, _) {
+  //(function ( $ ) {
+    'use strict';
+    var o = $({});
 
-  $.subscribe = function() {
-    o.on.apply(o, arguments);
-  };
+    $.subscribe = function() {
+      o.on.apply(o, arguments);
+    };
 
-  $.unsubscribe = function() {
-    o.on.apply(o. arguments);
-  };
+    $.unsubscribe = function() {
+      o.on.apply(o. arguments);
+    };
 
-  $.publish = function() {
-    o.trigger.apply(o, arguments);
-  };
+    $.publish = function() {
+      o.trigger.apply(o, arguments);
+    };
+      var resultTemplate = _.template($('#resultTemplate').html());
 
-}(jQuery));
+    $.subscribe('/search/tags', function (e, tags) {
+      $('#lastQuery').html('<p>Searched for: <strong>' + tags + '</strong></p>');
+      console.log(e);
+    });
 
-;(function ($) {
-  'use strict';
-  var resultTemplate = _.template($('#resultTemplate').html());
+    $.subscribe('search/resultSet', function (e, results) {
+      $('#searchResults').empty().append(resultTemplate(results));
+    });
 
-  $.subscribe("/search/tags", function (e, tags) {
-    $('#lastQuery').html('<p>Searched for: <strong>' + tags + '</strong></p>');
-    console.log(e);
-  });
+    $('#flickrSearch').submit(function (e) {
+      e.preventDefault();
+      var tags = $(this).find('#query').val();
 
-  $.subscribe("/search/resultSet", function (e, results) {
-    $("#searchResults").empty().append(resultTemplate(results));
-  });
-
-  $("#flickrSearch").submit(function (e) {
-    e.preventDefault();
-    var tags = $(this).find('#query').val();
-
-    if (!tags) {
-      return;
-    }
-    $.publish("/search/tags", [$.trim(tags)]);
-  });
-
-  $.subscribe("/search/tags", function (e, tags) {
-    $.getJSON( "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
-      tags: tags,
-      tagmode: "any",
-      format: "json"
-    },
-    function (data) {
-      if (!data.items.length) {
+      if (!tags) {
         return;
       }
-      $.publish("/search/resultSet", { items: data.items } );
+      $.publish('/search/tags', [$.trim(tags)]);
     });
-  });
-}(jQuery));
 
+    $.subscribe('/search/tags', function (e, tags) {
+      $.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?', {
+        tags: tags,
+        tagmode: 'any',
+        format: 'json'
+      },
+      function (data) {
+        if (!data.items.length) {
+          return;
+        }
+        $.publish('/search/resultSet', { items: data.items } );
+      });
+    });
+});
 
-angular.module('mainApp', [])
+require(['angular'], function (angular) {
+  'use strict';
+  angular.module('mainApp', [])
   .controller('MainController', ['$scope', '$q', '$interval', function($scope, $q, $interval) {
     // we will use $q here
     'use strict';
@@ -71,7 +68,7 @@ angular.module('mainApp', [])
 
         i = i+1;
 
-        if (i == 5) {
+        if (i === 5) {
           deferred.resolve('Counter has reached 5.');
           $interval.cancel(timer);
         } else {
@@ -93,29 +90,61 @@ angular.module('mainApp', [])
     }
   }]);
 
-angular.module('mainApp').factory('WeatherService', function($http) {
-  return {
-    getWeather: function(city, country) {
-      var query = city + ',' + country;
-      return $http.get('http://api.openweathermap.org/data/2.5/weather', {
-        params: {
-          q: query,
-        }
-      }).then(function (response) {
-        return response.data.weather[0];
+  angular.module('mainApp').factory('WeatherService', function($http) {
+    return {
+      getWeather: function(city, country) {
+        var query = city + ',' + country;
+        return $http.get('http://api.openweathermap.org/data/2.5/weather', {
+          params: {
+            q: query,
+          }
+        }).then(function (response) {
+          return response.data.weather[0];
+        });
+      }
+    }
+  });
+
+  angular.module('mainApp').controller('WeatherController', function($scope, WeatherService) {
+    $scope.getWeather = function() {
+      $scope.weatherDescription = 'Fetching...';
+      WeatherService.getWeather($scope.city, $scope.country).then(function(data) {
+        $scope.weatherDescription = data//.description;
+        $scope.weatherIcon = data.icon;
+      }, function() {
+        $scope.weatherDescription = 'Could not obtain data.';
       });
     }
-  }
+  });
+
+  angular.module('mainApp').factory('IPService', function ($http) {
+    return {
+      getIp: function() {
+        return $http.get('http://www.telize.com/geoip?callback=', function (data) {
+          console.log('The IP address is ' + data.ip);
+          console.log('Latitude: ' + data.latitude);
+          console.log('Longitude: ' + data.longitude);
+        }).then(function (response) {
+          return response.data;
+        });
+      }
+    }
+  });
+
+  angular.module('mainApp').controller('IPController', function($scope, IPService) {
+    $scope.getIp = function() {
+      $scope.ipAddress = 'Fetching...';
+      IPService.getIp().then(function (data) {
+        $scope.ipAddress = data.ip//.description;
+        $scope.latitude = data.latitude + '˚';
+        $scope.longitude = data.longitude + '˚';
+      }, function() {
+        $scope.ipAddress = 'Could not obtain data.';
+      });
+    }
+  });
+
+  //http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml
+
 });
 
-angular.module('mainApp').controller('WeatherController', function($scope, WeatherService) {
-  $scope.getWeather = function() {
-    $scope.weatherDescription = 'Fetching...';
-    WeatherService.getWeather($scope.city, $scope.country).then(function(data) {
-      $scope.weatherDescription = data//.description;
-      $scope.weatherIcon = data.icon;
-    }, function() {
-      $scope.weatherDescription = 'Could not obtain data.';
-    });
-  }
-});
---
+++ app/scripts/requireConfig.js
@@ -0,0 +1,57 @@
+/*globals require*/
+require.config({
+    paths: {
+        'jquery': '../bower_components/jquery/dist/jquery',
+        'velocity': '../bower_components/velocity/velocity',
+        'velocity-ui': '../bower_components/velocity/velocity.ui',
+        'blast': '../bower_components/blast-text/jquery.blast',
+        //'lodash': '../bower_components/lodash/dist/lodash',
+        'underscore': '../bower_components/underscore/underscore',
+        //'backbone': '../bower_components/backbone/backbone',
+        'angular': '../bower_components/angular/angular',
+        'angularRoute': '../bower_components/angular-route/angular-route',
+        'angularMocks': '../bower_components/angular-mocks/angular-mocks',
+        'text': '../bower_components/text/text'
+    },
+    shim: {
+        'velocity': {
+            'deps': ['jquery']
+        },
+        'velocity-ui': {
+            'deps': ['velocity']
+        },
+        'blast': {
+            'deps': ['jquery']
+        },
+        'angular' : {
+            'exports' : 'angular'
+        },
+        'angularRoute': ['angular'],
+        'angularMocks': {
+            deps: ['angular'],
+            exports: 'angular.mock'
+        },
+        'underscore': {
+            'exports': '_'
+        },
+        'jquery': {
+            'exports': '$'
+        }
+    },
+    packages: [
+
+    ],
+    priority: [
+    'angular'
+    ]
+});
+require([
+    'jquery',
+    'velocity',
+    'velocity-ui',
+    'blast',
+    'underscore',
+    'angular',
+    'angularRoute',
+    'text',
+    'main'], function($, Velocity, _, angular){});
