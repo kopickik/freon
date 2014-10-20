826f382fb8b6f153cd0c3f43966fad3565ef8bea
Kyle Kopicki <kyle.kopicki@gmail.com>
2014-10-20 10:04:52 -0400

    adding weatherService (openweathermap.org)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -92,3 +92,30 @@ angular.module('mainApp', [])
       });
     }
   }]);
+
+angular.module('mainApp').factory('WeatherService', function($http) {
+  return {
+    getWeather: function(city, country) {
+      var query = city + ',' + country;
+      return $http.get('http://api.openweathermap.org/data/2.5/weather', {
+        params: {
+          q: query,
+        }
+      }).then(function (response) {
+        return response.data.weather[0];
+      });
+    }
+  }
+});
+
+angular.module('mainApp').controller('WeatherController', function($scope, WeatherService) {
+  $scope.getWeather = function() {
+    $scope.weatherDescription = 'Fetching...';
+    WeatherService.getWeather($scope.city, $scope.country).then(function(data) {
+      $scope.weatherDescription = data//.description;
+      $scope.weatherIcon = data.icon;
+    }, function() {
+      $scope.weatherDescription = 'Could not obtain data.';
+    });
+  }
+});
